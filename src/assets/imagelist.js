627c48f5b131b3c9d121f1e10735547187048e17
coen-h <105825347+coen-h@users.noreply.github.com>
2024-08-08 12:17:28 +1200

    hero image swiper

--- src/assets/imagelist.js
+++ src/assets/imagelist.js
@@ -1,4 +1,5 @@
 const imageList = [
+    'https://evolocity.co.nz/wp-content/uploads/2021/11/Evolocity_WellingtonManawatu_RegionalFinals2021_KartRacingTrike-min-scaled-2560x1280.jpeg',
     'https://evolocity.co.nz/wp-content/uploads/2023/12/IMG_4460-1-scaled.jpeg',
     'https://evolocity.co.nz/wp-content/uploads/2023/12/Next-Gen-National-Rally-Day-2023-63-2-scaled.jpg',
     'https://evolocity.co.nz/wp-content/uploads/2023/11/DSC_0085.jpg',
@@ -7,7 +8,7 @@ const imageList = [
     'https://evolocity.co.nz/wp-content/uploads/2023/11/IMG_0910-scaled.jpeg',
     'https://evolocity.co.nz/wp-content/uploads/2023/11/IMG_0912-scaled.jpeg',
     'https://evolocity.co.nz/wp-content/uploads/2023/11/DSC1613-1.jpg',
-    'https://evolocity.co.nz/wp-content/uploads/2023/11/DSC1297.jpg',
+    // 'https://evolocity.co.nz/wp-content/uploads/2023/11/DSC1297.jpg',
     // 'https://evolocity.co.nz/wp-content/uploads/2023/11/20231021-nz-chch-055-1.jpg',
     // 'https://evolocity.co.nz/wp-content/uploads/2023/11/DSC_0199.jpg',
     // 'https://evolocity.co.nz/wp-content/uploads/2023/11/DSC_0109.jpg'
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import imageList from '../assets/imagelist'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+import 'swiper/css';
 
 export default function Hero() {
     return (
         <div id="hero">
-            <img id="hero-image" src="/1.jpeg" alt="Hero Image" />
+            <Swiper
+                spaceBetween={0}
+                centeredSlides={true}
+                autoplay={{
+                  delay: 3500,
+                  disableOnInteraction: false,
+                }}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[Autoplay, Pagination]}
+                id="hero-swiper"
+            >
+                {imageList.map((image) => (
+                    <SwiperSlide key={image}><img id="hero-image" src={image} alt="Hero Image" /></SwiperSlide>
+                ))}
+                
+            </Swiper>
             <p id="hero-title" >Evolocity</p>
             <div id="hero-description"><p>"EVolocity is the EV programme inspiring young Kiwis into sustainable
             engineering while fast forwarding New Zealand’s clean tech future."</p><a href="https://evolocity.co.nz/" target="_blank" aria-label="Website" > - Evolocity</a></div>
